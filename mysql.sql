c75ec9c63fc865a44114e4c96bee25ff0cc13840
ZinaidaV <vzp2030@gmail.com>
2025-01-12 15:14:40 +0300

    Add files via upload

--- mysql.sql
+++ mysql.sql
@@ -1,28 +1,40 @@
 
---CREATE TABLE Users(UserID INTEGER NOT NULL PRIMARY KEY, Login TEXT NOT NULL, Type NOT NULL);
---INSERT INTO Users(UserID, Login, Type)
---VALUES (1, "Zinaida", "user");
+--CREATE TABLE Users(UserID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Login TEXT NOT NULL, Type NOT NULL);
+--INSERT INTO Users(Login, Type)
+--VALUES ("Tom", "user");
+--DELETE FROM Users
+--WHERE Login = "Tom";
 SELECT * FROM Users;
 
---CREATE TABLE Partners(PartnerID INTEGER NOT NULL PRIMARY KEY, Name TEXT NOT NULL, Contacts TEXT, Description TEXT);
---INSERT INTO Partners(PartnerID, Name, Contacts, Description)
---VALUES (1, "Буше", "phone number", "кондитерская");
+
+--CREATE TABLE Partners(PartnerID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , Name TEXT NOT NULL, Contacts TEXT, Description TEXT);
+--INSERT INTO Partners(Name, Contacts, Description)
+--VALUES ("Спортмастер", "phone number", "спортивный магазин");
+--UPDATE Partners SET Description = "Торговый центр" WHERE Name = "Мега";
+
 SELECT * FROM Partners;
 
---CREATE TABLE Places(PlaceID INTEGER NOT NULL PRIMARY KEY, Partner INTEGER NOT NULL, Adress TEXT, FOREIGN KEY(Partner) REFERENCES Partners(PartnerID));
---INSERT INTO Places(PlaceID, Partner, Adress)
---VALUES (1, "Буше", "адрес");
+--CREATE TABLE Places(PlaceID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Partner INTEGER NOT NULL, Adress TEXT, FOREIGN KEY(Partner) REFERENCES Partners(PartnerID));
+--INSERT INTO Places(Partner, Adress)
+--VALUES ("Мега", "адрес2");
 SELECT * FROM Places;
 
 --CREATE TABLE Reviews(User INTEGER NOT NULL, Place INTEGER NOT NULL, Contents TEXT NOT NULL, FOREIGN KEY(User) REFERENCES Users(UserID), FOREIGN KEY(Place) REFERENCES Places(PlaceID));
 --INSERT INTO Reviews(User, Place, Contents)
---VALUES (1, 1, "great!");
+--VALUES (2, 1, "perfect!");
 SELECT * FROM Reviews;
 
 --CREATE TABLE Sales(Partner INTEGER NOT NULL, Amount TEXT NOT NULL, Description TEXT, FOREIGN KEY(Partner) REFERENCES Partners(PartnerID));
 --INSERT INTO Sales(Partner, Amount, Description)
---VALUES (1, "20%", "every evening after 8pm");
+--VALUES (3, "3%", "in a sportmaster");
 SELECT * FROM Sales;
+
+
+
+
+
+
+
 
 
 
