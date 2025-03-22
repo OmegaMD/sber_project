ef598e2f0451ce516cec790cd86db12e2f181f21
crossity <savvapos2008@gmail.com>
2025-03-22 23:29:25 +0300

    added map's rough search bar version

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ class App:
         ### 2gis api key ###
         self.TWOGIS_API_KEY = 'ab7b70c9-9132-468f-8b4c-87177a2418cf'
 
-        ### flask callback functions ###
+        ### flask pages callback functions ###
 
         # home flask function
         @self.flask.route('/')
@@ -73,51 +73,82 @@ class App:
             # user info setup
             # self.set_var("user", self.database.access_user("guest"))
             session["user"] = pickle.dumps(self.database.access_user("guest"))
+            session["last_location_search"] = ""
 
             # getting browser info
             user_agent = request.headers.get('User-Agent')
             print("nigga0")
             return render_template('map.html')
 
-
-        @self.flask.route('/cafes/<float:lat>/<float:lon>')
-        def get_cafes(lat, lon):
-            print('nigga2')
-            url = f"https://catalog.api.2gis.com/3.0/items"
-            params = {
-                'key': self.TWOGIS_API_KEY,
-                'point': f"{lon},{lat}",
-                'radius': 1000,  # радиус поиска в метрах
-                'q': 'кафе',
-                'limit': 10,
-                'fields': 'items.point'
-            }
-            response = requests.get(url, params=params)
-
-            if response.status_code != 200:
-                return jsonify({"error": "Failed to fetch data from 2GIS"}), 500
-
-            data = response.json()
-
-            cafes = []
-            if 'result' in data:
-                for item in data['result']['items']:
-                    cafes.append({
-                        'name': item['name'],
-                        'address': item['address_name'],
-                        'point': item['point']
-                    })
-
-            print(cafes)
-            
-            return jsonify(cafes)
-
+        # main page callback function
         @self.flask.route('/main_page', methods=['POST'])
         def main():
             print(pickle.loads(self.get_var("user")))
             return render_template('home.html',
                                    user=pickle.loads(self.get_var("user")),
                                    top_discount_partners=self.database.access_top_discount_partners())
+
+        # map page flask callback function
+        @self.flask.route('/map')
+        def map():
+            return render_template('map.html', locations=[])
+
+        ### flask inner callback functions ###
+
+        # user location saving function
+        @self.flask.route('/save_user_location/<float:lat>/<float:lon>')
+        def save_user_location(lat, lon):
+            session["lat"] = lat
+            session["lon"] = lon
+
+            print(lat)
+            print(lon)
+            return "200"
+
+        # flask location search bar callback function
+        @self.flask.route('/location_search', methods=['POST'])
+        def location_search():
+            user_input = request.form['location_search_bar']
+            session["last_location_search"] = user_input
+
+            if user_input != '':
+                locations = search_closest_locations(session['lat'], session['lon'], user_input)
+                return render_template('map.html', locations=locations)
+            return redirect(url_for('map'))
+
+        ### help functions ###
+
+        # closest locations based on query searching function
+        def search_closest_locations(lat, lon, query):
+            url = f"https://catalog.api.2gis.com/3.0/items"
+            params = {
+                'key': self.TWOGIS_API_KEY,
+                'point': f"{lon},{lat}",
+                # 'page_size': 30,
+                'radius': 1000,  # радиус поиска в метрах
+                'q': query,
+                'fields': 'items.point',
+                'sort': 'distance',
+                # 'ype': 'adm_div.city'
+            }
+            response = requests.get(url, params=params)
+
+            if response.status_code != 200:
+                print('2gis error')
+                return jsonify({"error": "Failed to fetch data from 2GIS"}), 500
+
+            data = response.json()
+
+            locations = []
+            if 'result' in data:
+                for item in data['result']['items']:
+                    locations.append({
+                        'name': item['name'].split(',')[0],
+                        'address': item['address_name'],
+                        'point': item['point']
+                    })
+            print(len(locations))
+            return locations
 
 # application instance
 app = App()
