c54408c6bd23e855a1cc855448b1c39bf7cc1f74
crossity <savvapos2008@gmail.com>
2025-03-27 20:28:36 +0300

    started creating filters window

--- static/map.js
+++ static/map.js
@@ -39,3 +39,31 @@ function pop_location_info() {
     box.style.padding = "0";
     box.innerHTML = '';
 }
+
+function show_filters() {
+    const box = document.getElementById('location_info_box');
+    box.style.padding = "10px";
+    const new_element = document.createElement('div');
+    new_element.className = 'block appearing';
+    new_element.style.overflow = 'hidden';
+    // new_element.innerHTML = '<img class="" style="width: 100%" src="' + img_url + '"></img>';
+
+    const img = document.createElement('img');
+    img.src = 'https://wallpapers.com/images/featured/cool-skull-pictures-oi8h4846tbciuosj.webp';
+    img.style.width = "100%";
+    img.style.maxWidth = "none";
+    img.style.maxHeight = "none";
+    img.style.borderRadius = "20px";
+
+    new_element.appendChild(img);
+
+    box.appendChild(new_element);
+
+    img.onload = function() {
+        new_element.style.height = new_element.scrollHeight * 2 + 'px';
+
+        setTimeout(() => {
+            new_element.classList.add('show');
+        }, 10);
+    };
+}
